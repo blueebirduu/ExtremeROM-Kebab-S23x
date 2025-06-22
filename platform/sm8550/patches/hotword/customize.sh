87e01d5a28aaa6b38d0fc7b46af3c994559d470d
osrott61-gh <osrott61@dev.extremerom.org>
2025-06-22 11:17:20 +0200

    sm8550: add various patches
    Signed-off-by: osrott61-gh <osrott61@dev.extremerom.org>

--- platform/sm8550/patches/hotword/customize.sh
+++
@@ -1,7 +0,0 @@
-echo "Replacing Hotword"
-DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentXGoogleEx6_WIDEBAND_LARGE"
-DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentYGoogleEx6_WIDEBAND_LARGE"
-if [[ "$TARGET_CODENAME" != "dm3q" ]]; then
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentOKGoogleEx4HEXAGON/HotwordEnrollmentOKGoogleEx4HEXAGON.apk" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentXGoogleEx4HEXAGON/HotwordEnrollmentXGoogleEx4HEXAGON.apk" 0 0 644 "u:object_r:system_file:s0"
-fi    
